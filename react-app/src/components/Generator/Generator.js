c0db10a4459331a5709f57a858804fbb15b90c3f
Ty <ltgray579@gmail.com>
2021-02-12 03:33:26 -0700

    end of day push, collecting form data still

--- react-app/src/components/Generator/Generator.js
+++ react-app/src/components/Generator/Generator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Generator.css'
 
 import whiteSymbol from './mana-symbols/White_Mana.png'
@@ -8,33 +8,38 @@ import redSymbol from './mana-symbols/Red_Mana.png'
 import greenSymbol from './mana-symbols/Green_Mana.png'
 
 const Generator = () => {
+
+    const [white, setWhite] = useState(false);
+    const [blue, setBlue] = useState(false);
+
+
     return(
         <div id='generator-wrapper'>
             <form id='g-form'>
                 <div id='color-checkboxes'>
-                    <input type='checkbox' id='white-selector' name='white-selector' value='white' />
-                    <label for='white-selector'><img src={whiteSymbol} /></label>
-                    <input type='checkbox' id='blue-selector' name='blue-selector' value='blue' />
-                    <label for='blue-selector'><img src={blueSymbol} /></label>
+                    <input type='checkbox' id='white-selector' name='white-selector' value='white' onChange={() => white ? setWhite(false) : setWhite(true)}/>
+                    <label htmlFor='white-selector'><img src={whiteSymbol} alt='mana_symbol' /></label>
+                    <input type='checkbox' id='blue-selector' name='blue-selector' value='blue' onChange={() => blue ? setBlue(false) : setBlue(true)} />
+                    <label htmlFor='blue-selector'><img src={blueSymbol} alt='mana_symbol' /></label>
                     <input type='checkbox' id='black-selector' name='black-selector' value='black' />
-                    <label for='black-selector'><img src={blackSymbol} /></label>
+                    <label htmlFor='black-selector'><img src={blackSymbol} alt='mana_symbol' /></label>
                     <input type='checkbox' id='red-selector' name='red-selector' value='red' />
-                    <label for='red-selector'><img src={redSymbol} /></label>
+                    <label htmlFor='red-selector'><img src={redSymbol} alt='mana_symbol' /></label>
                     <input type='checkbox' id='green-selector' name='green-selector' value='green' />
-                    <label for='green-selector'><img src={greenSymbol} /></label>
+                    <label htmlFor='green-selector'><img src={greenSymbol} alt='mana_symbol' /></label>
                 </div>
                 <div id='color-devotion'>
-                    <input type='text' id='white-devotion' />
-                    <input type='text' id='blue-devotion' />
-                    <input type='text' id='black-devotion' />
-                    <input type='text' id='red-devotion' />
-                    <input type='text' id='green-devotion' />
+                    <input type='number' id='white-devotion' />
+                    <input type='number' id='blue-devotion' />
+                    <input type='number' id='black-devotion' />
+                    <input type='number' id='red-devotion' />
+                    <input type='number' id='green-devotion' />
                 </div>
 
-                <label for='num-of-lands'>Number of lands</label>
+                <label htmlFor='num-of-lands'>Number of lands</label>
                 <input type='text' id='number-of-lands' name='num-of-lands' />
 
-                <label for='format'>Select your format</label>
+                <label htmlFor='format'>Select your format</label>
                 <select name='format' id='format-selector'>
                     <option value='standard'>Standard</option>
                     <option value='commander'>Commander</option>
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,36 +1,80 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import '../index.css'
+
+// const mySideNav = React.forwardRef((props, red) => {
+//   return <div id="mySidenav" className="sidenav-container"></div>
+// });
+
+// const Backdrop = React.forwardRef((props, ref) => {
+//   return <div className="backdrop-container" id="backdrop"></div>
+// });
 
 const NavBar = ({ setAuthenticated }) => {
+
+  // const mySideNav = useRef(null);
+  // const backdrop = useRef(null);
+
+  // function openNav() {
+  //     document.getElementById("mySidenav").style.width = "33%" //opens side navbar by 70 percent
+  //     document.getElementById('backdrop').style.display = "block" //displays overlay
+  // }
+
+  // function closeNav() {
+  //     mySideNav.style.width = "0"
+  //     document.getElementById('backdrop').style.display = "none"
+  // }
+
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/generator" exact={true} activeClassName="active">
-            Generator
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton setAuthenticated={setAuthenticated} />
-        </li>
-      </ul>
-    </nav>
+    <div>
+      {/* <mySideNav ref={mySideNav} id="mySidenav" className="sidenav-container">
+
+        <span className="drawer-close-button">
+          <a href="javascript:void(0)" className="closebtn" onclick={closeNav()}>&times;</a>
+        </span>
+
+        <NavLink to="/" onclick={closeNav()} id="home-link">Home</NavLink>
+        <NavLink to="/generator" onclick={closeNav()} id="about-link">Generator</NavLink>
+        <NavLink to="/login" onclick={closeNav()} id="works-link">Login</NavLink>
+        <NavLink to="/signup" onclick={closeNav()} id="contact-link">Signup</NavLink>
+
+      </mySideNav>
+
+      <Backdrop ref={backdrop} className="backdrop-container" id="backdrop" /> */}
+
+      <nav>
+        {/* <div id='span-nav'>
+          <span onclick={openNav()} className="mobile-nav-open-icon">&#9776;</span>
+        </div> */}
+        <ul>
+          <li>
+            <NavLink to="/" exact={true} activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/generator" exact={true} activeClassName="active">
+              Generator
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" exact={true} activeClassName="active">
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/sign-up" exact={true} activeClassName="active">
+              Sign Up
+            </NavLink>
+          </li>
+          <li>
+            <LogoutButton setAuthenticated={setAuthenticated} />
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
