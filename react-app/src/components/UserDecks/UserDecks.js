c082f6015bf13d73aacd298bd76a9bd9a22843d7
Ty <ltgray579@gmail.com>
2021-02-25 22:53:02 -0700

    deck select styled

--- react-app/src/components/UserDecks/UserDecks.js
+++ react-app/src/components/UserDecks/UserDecks.js
@@ -1,29 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { authenticate } from '../../services/auth';
-// import './Home.css'
+import './UserDecks.css'
 
 import {fetchUserDecks} from '../../services/decks';
 
 const UserDecks = () => {
 
+    const [loaded, setLoaded] = useState(false)
+    const [returnedDecks, setReturnedDecks] = useState({})
     let userId;
-    let returnedDecks;
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+      }
+
 
     useEffect(() => {
         (async() => {
             const user = await authenticate();
             if (!user.errors){
                 userId = user.id;
-                returnedDecks = await fetchUserDecks(userId)
-                console.log(returnedDecks);
+                let response = await fetchUserDecks(userId)
+                setReturnedDecks(response);
+                console.log(JSON.parse(response.decks[0].color))
             }
+            setLoaded(true);
         })();
     }, []);
+
+    if (!loaded){
+        return null
+    }
 
     return(
         <div id='decks-wrapper'>
             <div id='deck-select'>
-
+                <ul>
+                    {loaded ? returnedDecks.decks.map((deck) => {
+                        return <li className={`${JSON.parse(deck.color)[getRandomInt(JSON.parse(deck.color).length)]}`}>{deck.name}</li>
+                    }) : <li>Loading</li>}
+                    <li className='Blue'>Mono Blue</li>
+                    <li className='Black'>Mono Black</li>
+                    <li className='White'>Mono White</li>
+                    <li id='new-deck'>+</li>
+                </ul>
             </div>
             <div id='deck-details'>
 
