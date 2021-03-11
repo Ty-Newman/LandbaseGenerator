f45b8dd411402bc7312883b5484dac9880b3e4e1
Ty <ltgray579@gmail.com>
2021-03-10 17:47:49 -0700

    setting up component for creating decks

---
+++ react-app/src/components/UserDecks/CreateDeck.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+import { authenticate } from '../../services/auth';
+import './UserDecks.css'
+
+const CreateUserDeck = () => {
+
+}
+
+export default CreateUserDeck;
